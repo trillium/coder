aad178e6487ae5b4dca9306afc84794cbc13fd50
Ethan Dickson <ethan@coder.com>
2026-09-16 06:47:43 +0000

    fix(coderd/x/chatd): pause the task watchdog while a model stream is open

--- coderd/x/chatd/options.go
+++ coderd/x/chatd/options.go
@@ -210,7 +210,6 @@ type chatWorkerOptions struct {
 	AcquisitionWakeChannelSize int
 	TaskRetryInitialBackoff    time.Duration
 	TaskRetryMaxBackoff        time.Duration
-	TaskTimeout                time.Duration
 }
 
 func (o chatWorkerOptions) withDefaults() (chatWorkerOptions, error) {
